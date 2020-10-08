16efd48fab476b6a00a6441232b1071a032bd3e7
Deeksha Amin <diya-deekshaa@CPU1595.local>
2020-10-08 20:02:41 +0530

    poker first page styles updated

--- src/components/people-list/index.js
+++ src/components/people-list/index.js
@@ -7,6 +7,12 @@ const PeopleList = ({ data }) => {
             <div className="people_list boxbg container">
                 <div className="row heading">
                     Members
+                    <div className="action-icons">
+                      <ul>
+                        <li><button><i class="fa fa-user-plus" aria-hidden="true"></i></button></li>
+                        <li><button><i class="fa fa-user-times" aria-hidden="true"></i></button></li>
+                      </ul>
+                    </div>
                 </div>
                 <ul className="list">
                     {
