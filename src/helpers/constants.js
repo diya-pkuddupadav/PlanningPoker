39130d0e9e6b3f2fdd6c1a957b0dc094416cd851
diya-pkuddupadav <praveen.ku@endurance.com>
2020-10-09 14:24:09 +0530

    Update constants.js

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,12 +1,12 @@
 const data = {
-    "client_id": "Usw2gA0sFPvl9snuyTqTdw6yNRcCI4ng",
-    "client_secret":"fkAX03ILXCbH5sIFiaVDeC3J_Hy1iZrL5gPaKN8zmMqQ32J5GTQ-Z3OAC_91t3xS",
-    "Callback_URL":"https://9e6d2e3f5870.ngrok.io",
+    "client_id": "BRphak8h3YqAhP67Jxs0XSnmzrqfYlQn",
+    "client_secret":"MFITDWbKbGcWIse1wP3h247LLIGwSl032o4ebP18k-x2AeCgZVeLHEInh6TpwZL8",
+    "Callback_URL":"https://atlas-planning-poker.herokuapp.com",
     "YOUR_USER_BOUND_VALUE":"",
     "API":{
         getAuth:'https://auth.atlassian.com/oauth/token'
     },
-    "backendURL": "http://localhost:5000",
+    "backendURL": "https://planningpockernency.herokuapp.com",
     
 }
 data.REST_API_authorization_URL = `https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=${data.client_id}&scope=write%3Ajira-work%20read%3Ame%20read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20manage%3Ajira-data-provider%20read%3Ajira-user%20report:personal-data&redirect_uri=${data.Callback_URL}&state=${data.YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent`
